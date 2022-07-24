ece84c66da77f771cc73af005a7f1d54a26e5393
Huanchen Zhai <hczhai@ucla.edu>
2022-07-23 20:10:43 -0700

    using mkl

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,17 @@
 
 #include <main.hpp>
 #include <iostream>
+#include <vector>
+#include <iomanip>
 
 using namespace std;
 
 int main() {
     cout << add<int>(5, 4) << endl;
     cout << add<double>(5.1, 4.1) << endl;
+    vector<double> a = {5.3, 1.2, 5.4};
+    vector<double> b = {0.1, -7.4, 2.3};
+    cout << fixed << setprecision(15) << xdot(a.data(), b.data(), (MKL_INT)a.size()) << endl;
+    cout << fixed << setprecision(15) << mydot(a.data(), b.data(), (MKL_INT)a.size()) << endl;
     return 0;
 }
--- src/main.hpp
+++ src/main.hpp
@@ -1,5 +1,50 @@
 
-template<typename T>
-T add(T a, T b) {
+#ifdef _HAS_INTEL_MKL
+#ifndef MKL_Complex8
+#include <complex>
+#define MKL_Complex8 std::complex<float>
+#endif
+#ifndef MKL_Complex16
+#include <complex>
+#define MKL_Complex16 std::complex<double>
+#endif
+#include "mkl.h"
+#else
+#ifdef MKL_ILP64
+#define MKL_INT long long int
+#else
+#define MKL_INT int
+#endif
+#endif
+
+extern "C"
+{
+
+#ifndef _HAS_INTEL_MKL
+
+    // vector dot product
+    extern double ddot(const MKL_INT *n, const double *dx, const MKL_INT *incx,
+                       const double *dy, const MKL_INT *incy) noexcept;
+
+#endif
+}
+
+template <typename T>
+T add(T a, T b)
+{
     return a + b;
+}
+
+double xdot(double *a, double *b, MKL_INT n)
+{
+    MKL_INT inc = 1;
+    return ddot(&n, a, &inc, b, &inc);
+}
+
+double mydot(double *a, double *b, MKL_INT n)
+{
+    double r = 0;
+    for (MKL_INT i = 0; i < n; i++)
+        r += a[i] * b[i];
+    return r;
 }
