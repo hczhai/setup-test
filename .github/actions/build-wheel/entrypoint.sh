70107953633980b0569cb94331f941e7d5168466
Huanchen Zhai <hczhai@ucla.edu>
2022-07-23 22:05:32 -0700

    fix rpath python version

--- .github/actions/build-wheel/entrypoint.sh
+++ .github/actions/build-wheel/entrypoint.sh
@@ -46,8 +46,10 @@ fi
 
 sed -i '/new_soname = src_name/a \    if any(x in src_name for x in ["libmkl_avx2", "libmkl_avx512"]): new_soname = src_name' \
     $($(cat $(which auditwheel) | head -1 | awk -F'!' '{print $2}') -c "from auditwheel import repair;print(repair.__file__)")
-sed -i '/rpath_set\[rpath\]/a \    import site\n    for x in set(["../lib" + p.split("lib")[-1] for p in site.getsitepackages()]): rpath_set[rpath.replace("../..", x)] = ""' \
+${PY_EXE} -c 'import site; print("*".join(site.getsitepackages()))' > ptmp
+sed -i '/rpath_set\[rpath\]/a \    import site\n    for x in set(["../lib" + p.split("lib")[-1] for p in open("ptmp").read().split("*")]): rpath_set[rpath.replace("../..", x)] = ""' \
     $($(cat $(which auditwheel) | head -1 | awk -F'!' '{print $2}') -c "from auditwheel import repair;print(repair.__file__)")
+rm ptmp
 
 /opt/python/"${PY_VER}"/bin/pip wheel . -w ./dist --no-deps
 
